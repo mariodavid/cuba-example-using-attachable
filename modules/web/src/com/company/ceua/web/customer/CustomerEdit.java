e243bd465091c04084b536b02a906a0ef496db0c
Mario <mario@die-davids.de>
2020-01-28 14:48:18 +0100

    switching to 0.7.1-SNAPSHOT for using new fragment

--- modules/web/src/com/company/ceua/web/customer/CustomerEdit.java
+++ modules/web/src/com/company/ceua/web/customer/CustomerEdit.java
@@ -1,11 +1,12 @@
 package com.company.ceua.web.customer;
 
-import com.haulmont.cuba.gui.screen.*;
 import com.company.ceua.entity.Customer;
+import com.haulmont.cuba.gui.screen.*;
 
 @UiController("ceua$Customer.edit")
 @UiDescriptor("customer-edit.xml")
 @EditedEntityContainer("customerDc")
 @LoadDataBeforeShow
 public class CustomerEdit extends StandardEditor<Customer> {
+
 }
